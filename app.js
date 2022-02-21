bee66043ac87054d6e5f0072c6403cc6d26e1cc9
SKSafin <81480983+SKSafin@users.noreply.github.com>
2022-02-21 09:34:02 +0600

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var sum = 0, num = 0, historyStr = "", historyPrintStr = "";
-function printHistory() {
-    document.getElementById("history-value").innerText = historyPrintStr;
+var sum = 0, num = 0, syntax = "", syntaxToPrint = "";
+function printsyntax() {
+    document.getElementById("syntax-value").innerText = syntaxToPrint;
 }
 function getOutput() {
     return reverseFormat(document.getElementById("output-value").textContent);
@@ -16,36 +16,51 @@ const operators = document.querySelectorAll(".operator");
 operators.forEach(x=> {
     x.addEventListener("click", function() {
         if(x.id == "clear") {
-            historyStr = "";
-            historyPrintStr = "";
+            syntax = "";
+            syntaxToPrint = "";
             sum = "";
             num = "";
-            printHistory();
+            printsyntax();
             printOutput(sum);
         }
         else if(x.id == "backspace") {
             let output = reverseFormat(getOutput());
             output = output.toString().slice(0, output.length - 1);
+            num = num.toString().slice(0, num.length - 1);
             printOutput(output);
         }
-        else {
+        else if(x.id == "equals") {
             let output = reverseFormat(getOutput());
-            if(output != 0) {
-                if(x.id == "equals") {
-                    sum = eval(historyStr + output);
-                    historyStr = "";
-                    historyPrintStr = "";
-                    printHistory();
-                    printOutput(sum);
-                    num = "";
-                }else {
-                    historyStr = historyStr + output + x.id;
-                    historyPrintStr = historyPrintStr + output + x.textContent;
+            sum = eval(syntax + output);
+            syntax = "";
+            syntaxToPrint = "";
+            printsyntax();
+            printOutput(sum);
+            num = "";
+        }else {
+            let output = reverseFormat(getOutput());
+            if(output == 0 && syntax != ""){
+                syntax = syntax.slice(0, syntax.length-1) + x.id;
+                syntaxToPrint = syntaxToPrint.slice(0, syntaxToPrint.length-1) + x.textContent;
+                printsyntax();
+            }
+            else if(output != 0) {
+                if(x.id == "%") {
+                    let pcOutput = output/100;
+                    syntax = syntax + pcOutput + "*";
+                    syntaxToPrint = syntaxToPrint + output + "%";
                     output = "";
-                    printHistory();
+                    printsyntax();
                     printOutput(output);
                     num = "";
-                }
+                }else {
+                    syntax = syntax + output + x.id;
+                    syntaxToPrint = syntaxToPrint + output + x.textContent;
+                    output = "";
+                    printsyntax();
+                    printOutput(output);
+                    num = "";
+                } 
             }
         }
     })
